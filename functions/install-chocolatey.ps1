3b3b5d9d8c317a13e45e4ee5b5e18fb9de6bb70c
unilock <haydena9448@gmail.com>
2020-11-25 15:12:52 -0500

    Create functions/install-chocolatey.ps1
    Shamelessly stolen from https://github.com/jayharris/dotfiles-windows

---
+++ functions/install-chocolatey.ps1
@@ -0,0 +1,19 @@
+function which($name) {
+    Get-Command $name -ErrorAction SilentlyContinue | Select-Object Definition
+}
+
+function Refresh-Environment {
+    $locations = 'HKLM:\SYSTEM\CurrentControlSet\Control\Session Manager\Environment',
+                 'HKCU:\Environment'
+
+    $locations | ForEach-Object {
+        $k = Get-Item $_
+        $k.GetValueNames() | ForEach-Object {
+            $name  = $_
+            $value = $k.GetValue($_)
+            Set-Item -Path Env:\$name -Value $value
+        }
+    }
+
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path","Machine") + ";" + [System.Environment]::GetEnvironmentVariable("Path","User")
+}
