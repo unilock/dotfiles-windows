4a0c37061dde02abc717b1b33961c4b6b15fd72f
unilock <unilock@fennet.rentals>
2021-10-15 14:25:17 -0400

    Why did I do this in the first place.
    Signed-off-by: unilock <unilock@fennet.rentals>

--- functions/common.ps1
+++ functions/common.ps1
@@ -2,6 +2,6 @@ function Print-Line {
 	param (
 		[string]$_line
 	)
-	$_cur_time = ((Get-Date -UFormat '%Y-%m-%d %H:%M:%S') | Out-String).Trim()
+	$_cur_time = Get-Date -UFormat '%Y-%m-%d %H:%M:%S'
 	Write-Output "[$_cur_time] $_line"
 }
